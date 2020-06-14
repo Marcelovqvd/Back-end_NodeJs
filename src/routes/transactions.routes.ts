079e9e20f050fc34bc3355f3b7d03c1a43f4fa9e
Marcelo <marcelovqvd@gmail.com>
2020-06-14 13:04:55 -0300

    code refactoring transactions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,44 +1,28 @@
 import { Router, request, response } from 'express';
-
-import Transactions from '../models/transaction';
-import TransactionsRepository from '../repositories/TransactionsRepository';
+import { uuid } from 'uuidv4';
 
 const transactionsRouter = Router();
-const transactionsRepository = new TransactionsRepository();
+
+const transactions = [];
 
 transactionsRouter.get('/', (request, response) => {
-  const transactions = transactionsRepository.all();
   return response.json(transactions);
-});
+})
 
 transactionsRouter.post('/', (request, response) => {
-    const { value, description, type, installments, card, date } = request.body;
+  const { consumer, type, value, date } = request.body;
 
-    const transaction = transactionsRepository.create({
-      date,
-      value,
-      description,
-      type,
-      installments,
-      card,
-    })
+  const transaction = {
+    id: uuid(),
+    consumer,
+    type,
+    value,
+    date
+  }
 
-    return response.json(transaction);
-});
+  transactions.push(transaction);
 
-transactionsRouter.get('/', (request, response) => {
-  return response.json({
-    "value": 100.00, // Valor da transação
-    "description": "Bicicleta ZXY Aro 21", // Descrição da transação
-    "type": "debit", // Tipo de transação (`debit`, `credit`, `installment_credit`)
-    "installments": null, // Número de parcelas, caso seja debito, passar `null`
-    "card": {
-      "number": "5200555500001234", // Número do cartão
-      "expiry": "20/21", // Validade do cartão
-      "cvv": "123", // Código de verificação do cartão
-      "holder": "Fulano de tal" // Nome do portador do cartão
-    }
-  });
-});
+  return response.json(transaction)
+})
 
 export default transactionsRouter;
